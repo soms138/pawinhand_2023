7fff6d541001ab5480d54813e67f836b8f919823
somin <thals0313@naver.com>
2023-07-28 17:37:05 +0900

    swiper

--- script/script.js
+++ script/script.js
@@ -26,3 +26,31 @@ close.addEventListener('click',function(){
     // 3-2. 모바일 메뉴 숨기기
     m_nav_open.style.display = 'none';
 })
+
+// main-swiper-slide
+// const 변수명 = new Swiper('적용대상');
+// const 변수명 = new Swiper('적용대상',{속성:값, 속성:{추가속성값});
+const pawin_slide = new Swiper('#pawin_slide',{
+    autoplay:{
+        delay:3000, // 슬라이드 간격(밀리초) 기본3초
+        disableOnInteraction:false //버튼 클릭 후 자동재생 유지
+    }, // 자동재생
+    loop:true, // 자동재생 시 한 방향으로 움직이도록 하는 기능 false 기본값
+    effect:'fade', // 제자리 변경 슬라이드 효과
+    // direction:'vertical'
+    navigation: { // 이전, 다음 내비게이션 연결
+        // next,prev 객체 연결 시 부모를 안적으면
+        // body 안에 있는 모든 swiper-next,prev를 인식하기 때문에
+        // 개별인식가능한 부모이름을 반드시 앞에 먼저 작성한다.
+        nextEl: '#pawin_slide .swiper-button-next',
+        prevEl: '#pawin_slide .swiper-button-prev',
+    }
+});
+const s_slide = new Swiper('#s_slide',{
+    autoplay:{delay:2000},
+    loop:true,
+    navigation: { // 이전, 다음 내비게이션 연결
+        nextEl: '#s_slide .swiper-button-next',
+        prevEl: '#s_slide .swiper-button-prev',
+    }
+})
